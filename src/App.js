a6d9f07fa604b5af0d423158aeac09be18110efe
Gan Eng Chin <engchin.gan@gmail.com>
2017-08-03 16:51:24 +0800

    position the clock to be in the center of the page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Clock from './Clock/Clock';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const appStyle = {
+      width: '100%',
+      height: '100%'
+    }
+    
+    const containerStyle = {
+      display: 'flex',
+      flex: 'auto',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%'
+    };
+    
+    const itemStyle = {
+      display: 'flex',
+      flex: 'auto',
+      flexDirection: 'row',
+      alignItems: 'center',
+    };
+
     return (
-      <div className="App">
-        <Clock></Clock>
+      <div className="App" style={appStyle}>
+        <div className="flex-container" style={containerStyle} >
+          <div className="flex-item" style={itemStyle}>
+            <Clock></Clock>
+          </div>
+        </div>
       </div>
     );
   }
